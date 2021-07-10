54615919d1da56f27d1175c9eae501cd3802e386
JoanStark <joanorba@gmail.com>
2021-07-10 11:04:17 +0200

    SpaceshipController

---
+++ Spaceship-Survival/Assets/Scripts/SpaceshipController/SpaceShipController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceShipSurvival
+{
+    public class SpaceShipController : MonoBehaviour
+    {
+        private Rigidbody2D rigidbody2D;
+        [SerializeField] private float movementSpeed = 30.0f;
+
+        private float inputX;
+        private float inputY;
+        private void Awake()
+        {
+            rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            inputX = Input.GetAxis("Horizontal");
+            inputY = Input.GetAxis("Vertical");
+        }
+
+        private void FixedUpdate()
+        {
+            rigidbody2D.AddForce(new Vector2(inputX, inputY) * (Time.deltaTime * movementSpeed), ForceMode2D.Impulse);
+        }
+    }
+}
