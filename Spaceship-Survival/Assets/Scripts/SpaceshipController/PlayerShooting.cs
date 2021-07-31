ee2ddbf390a638af87ed9ef18b8bd3fa182bdddf
Gasofa06 <87223808+Gasofa06@users.noreply.github.com>
2021-07-31 10:33:55 +0200

    Shooting Sytstem

--- Spaceship-Survival/Assets/Scripts/SpaceshipController/PlayerShooting.cs
+++ Spaceship-Survival/Assets/Scripts/SpaceshipController/PlayerShooting.cs
@@ -5,23 +5,65 @@ using UnityEngine;
 
 namespace  SpaceShipSurvival
 {
+
     public class PlayerShooting : MonoBehaviour
     {
         public GameObject bulletPrefab;
+        public GameObject bulletBox; //is where all the bullets will be kept
+
+        private GameObject activeBullet;
+        private GameObject instantiatedBullet;
+
+        public int numberOfUsingBullets = 0;
+
+        public int initialBullets;
 
         [SerializeField] public float timeBetweenBullets;
         private float betweenBulletsTimer;
 
+        private void Awake()
+        {
+            for (int i = 0; i < initialBullets; i++)
+            {
+                instantiatedBullet = Instantiate(bulletPrefab);
+
+                instantiatedBullet.SetActive(false);
+                instantiatedBullet.transform.parent = bulletBox.transform;
+            }
+        }
+
         private void Update()
         {
             betweenBulletsTimer -= Time.deltaTime;
-            
+
             if (Input.GetMouseButton(0) && betweenBulletsTimer <= 0)
             {
-                Instantiate(bulletPrefab, transform.position, transform.rotation);
+
+                if (bulletBox.transform.childCount == numberOfUsingBullets)
+                {
+                    Spawn();
+                }
+
+                activeBullet = bulletBox.transform.GetChild(numberOfUsingBullets).gameObject;
+
+                activeBullet.transform.position = transform.position;
+                activeBullet.transform.rotation = transform.rotation;
+                activeBullet.SetActive(true);
+
+                numberOfUsingBullets++;
+
                 betweenBulletsTimer = timeBetweenBullets;
+
+                activeBullet.transform.SetAsFirstSibling();
             }
         }
-    }
 
+        private void Spawn()
+        {
+            instantiatedBullet = Instantiate(bulletPrefab);
+
+            instantiatedBullet.SetActive(false);
+            instantiatedBullet.transform.parent = bulletBox.transform;
+        }
+    }
 }
