7f40d2c8c6b0f6fe81bc6e3d5b3fb8ec626aab2a
JoanOrtiga <joanorba@gmail.com>
2021-10-30 10:06:10 +0200

    camera

--- Spaceship-Survival/Assets/SimpleCameraFollower.cs
+++ Spaceship-Survival/Assets/SimpleCameraFollower.cs
@@ -7,8 +7,9 @@ namespace SpaceShipSurvival
     public class SimpleCameraFollower : MonoBehaviour
     {
         [SerializeField] private Transform _player;
-
         [SerializeField, Range(0.0f, 1.0f)] private float _speed;
+        
+        
         // Update is called once per frame
         void FixedUpdate()
         {
